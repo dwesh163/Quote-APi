1b893c9522dae549afad8e6fb53a129ff10a12f1
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-29 14:14:26 +0100

    [add] script

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs/promises');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/', async (req, res) => {
+	const content = req.body;
+
+	try {
+		await fs.access('quote.json');
+	} catch (error) {
+		await fs.writeFile('quote.json', '[]');
+	}
+
+	if (content.type === 'all') {
+		try {
+			const fileContent = await fs.readFile('quote.json', 'utf-8');
+			const quotes = JSON.parse(fileContent);
+			res.json(quotes);
+		} catch (error) {
+			res.status(500).send('Erreur lors de la lecture du fichier quote.json');
+		}
+	} else {
+		try {
+			const fileContent = await fs.readFile('quote.json', 'utf-8');
+			const quotes = JSON.parse(fileContent);
+			quotes.push(content);
+
+			await fs.writeFile('quote.json', JSON.stringify(quotes, null, 2));
+
+			res.send('OK');
+		} catch (error) {
+			res.status(500).send('Erreur lors de la manipulation du fichier quote.json');
+		}
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Serveur en écoute sur le port ${port}`);
+});
