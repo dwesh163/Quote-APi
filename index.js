09f71ce6ce68683c16cd4c50501f87c927b80129
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-12-29 17:06:04 +0100

    [add] delet methode

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs/promises');
 const { v4: uuidv4 } = require('uuid');
+require('dotenv').config();
 
 const app = express();
 const port = 1000;
+const deleteToken = process.env.DELETE_TOKEN;
 
 app.use(express.json());
 app.use(cors());
@@ -39,6 +41,30 @@ app.post('', async (req, res) => {
 				const randomIndex = Math.floor(Math.random() * quotes.length);
 				const randomQuote = quotes[randomIndex];
 				res.json(randomQuote);
+			}
+		} catch (error) {
+			res.status(500).send('Erreur lors de la manipulation du fichier quote.json');
+		}
+	} else if (content.type === 'supprimer') {
+		if (content.token !== deleteToken) {
+			res.status(401).send('Token incorrect. Autorisation refusée.');
+			return;
+		}
+
+		try {
+			const fileContent = await fs.readFile('quote.json', 'utf-8');
+			const quotes = JSON.parse(fileContent);
+
+			// Trouvez l'index de la citation avec l'id spécifié
+			const indexToRemove = quotes.findIndex((quote) => quote.id === content.id);
+
+			if (indexToRemove === -1) {
+				res.status(404).send('Citation non trouvée.');
+			} else {
+				// Supprimez la citation
+				quotes.splice(indexToRemove, 1);
+				await fs.writeFile('quote.json', JSON.stringify(quotes, null, 2));
+				res.send('Citation supprimée avec succès.');
 			}
 		} catch (error) {
 			res.status(500).send('Erreur lors de la manipulation du fichier quote.json');
